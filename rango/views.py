c618fddb83eb8aea48b6f3a71beb0d8548f259c5
M-frog123 <2612671w@student.gla.ac.uk>
2022-02-11 00:44:05 +0000

    chapter8

--- rango/views.py
+++ rango/views.py
@@ -19,7 +19,7 @@ def index(request):
     # that will be passed to the template engine.
     
     context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     
